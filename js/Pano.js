57f5110c9dd12540ac0f6c97562e196d692d6aa3
donkimber <kimber@fxpal.net>
2016-12-15 13:52:40 -0800

    added initial minimal code

---
+++ js/Pano.js
@@ -0,0 +1,184 @@
+
+function report(str)
+{
+    console.log(str);
+}
+
+function getClockTime()
+{
+    return new Date().getTime()/1000.0;
+}
+
+function getParameterByName(name) {
+    var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+}
+
+var photosphere = null;
+var imageUrl = null;
+var imNum = 0;
+var t0 = getClockTime();
+var numLoads = 0;
+var numManagerLoads = 0;
+var manager = null;
+var textureLoader = null;
+var prevTexture = null;
+var PS = null;
+
+THREE.TextureLoader.prototype.crossOrigin = '';
+THREE.ImageUtils.crossOrigin = '';
+
+function initPano(imgUrl, useGyro) {
+    if (imgUrl) {
+	imageUrl = imgUrl;
+    }
+    else {
+	imageUrl = "http://pollywss.paldeploy.com/getImage?camId=viewImage";
+    }
+    report("imageUrl: "+imageUrl);
+    manager = new THREE.LoadingManager();
+    textureLoader = new THREE.TextureLoader(manager);
+	  
+    //imageUrl = "stolanuten.jpg"
+    //imageUrl = "http://platonia:8000/getImage?camId=viewImage";
+    //imageUrl = "http://platonia/getImage?camId=viewImage";
+    //imageUrl = "http://pollywss.paldeploy.com/getImage?camId=viewImage";
+    //var speed = 0.00000001;
+    //var speed = getParameterByName('speed');
+    var speed = 0.01;
+    photosphere = THREE.Photosphere(document.getElementById('sphere'), imageUrl, {
+	view: getParameterByName('view'),
+	speed: speed,
+	useGyro: useGyro,
+	y: getParameterByName('y')
+    });
+    window.onresize = photosphere.resize;
+    PS = THREE.PhSp;
+    setTimeout(updateImage, 1000);
+}
+
+function setupDecorations()
+{
+    if (!PS) {
+	report("No photosphere initialized");
+    }
+    //var mat = new THREE.MeshBasicMaterial({});
+    //var mat = new THREE.MeshPhongMaterial({
+    //	color: 0xdaaddd, specular: 0x009900, shininess: 30, shading: THREE.FlatShading
+    //});
+    var redMat = new THREE.MeshBasicMaterial({
+	color: 0xff0000 , transparent: true, opacity: 0.9
+    });
+    var material = new THREE.MeshBasicMaterial({
+	color: 0xff6600 , transparent: true, opacity: 0.2
+    });
+    var sphereGeo = new THREE.SphereGeometry(0.5, 20, 20);
+    var sphere = new THREE.Mesh( sphereGeo, redMat );
+    report("pos: "+sphere.position);
+    sphere.scale.x = -1;
+    //sphere.position = new THREE.Vector3(5,2,1);
+    sphere.position.x = 5;
+    sphere.position.y = 0;
+    sphere.position.z = 2;
+    PS.scene.add( sphere );
+
+    var geometry = new THREE.BoxGeometry( 1.5, 10, 0.25 );
+    var box = new THREE.Mesh( geometry, material );
+    box.position.x = 5;
+    box.scale.x = -1;
+    PS.scene.add( box );
+
+    light1 = new THREE.PointLight( 0xff0040, 2, 50 );
+    light1.add( new THREE.Mesh( sphereGeo, new THREE.MeshBasicMaterial( { color: 0xff0040 } ) ) );
+    light1.position = new THREE.Vector3(5,5,5);
+    PS.scene.add( light1 );
+}
+
+function dump()
+{
+    report("sphere: "+photosphere);
+    report("texture: "+PS.texture);
+    report("material: "+PS.material);
+}
+
+function updateImage()
+{
+    imNum += 1;
+    //PS.controls.autoRotate = false;
+    //report("updatingImage...");
+    var url = imageUrl + "&imNumX="+imNum + "&t="+getClockTime();
+    //report("updateImage url: "+url);
+
+    textureLoader.load(url, function (texture) {
+	var t = getClockTime();
+	var dt = t - t0;
+	numLoads += 1;
+	//report("numLoads: "+numLoads+" "+numLoads/dt);
+        PS.material.map = texture;
+	//var prevTexture = PS.texture;
+        PS.texture = texture;
+        PS.material.map.needsUpdate = true; // probably bogus
+	PS.material.needsUpdate = true;
+	PS.sphere.needsUpdate = true;
+	PS.scene.needsUpdate = true;
+	//photosphere.render();
+	PS.renderer.render(PS.scene, PS.camera);
+	if (1 && prevTexture) {
+	    //report("deallocating texture");
+	    prevTexture.dispose();
+	}
+	prevTexture = texture;
+        setTimeout(updateImage, 5);
+    });
+}
+
+function setSurface(node)
+{
+    var name = PS.surface.name;
+    report("removing "+name);
+    PS.scene.remove(PS.surface);
+    PS.scene.add(node);
+    PS.surface = node;
+}
+
+function setSphere()
+{
+    PS.camera.position.x = 0.1;
+    PS.camera.position.y = 0;
+    PS.camera.position.z = 0;
+
+    setSurface(PS.sphere);
+}
+
+function setCirc()
+{
+    PS.camera.position.x = 0.1;
+    PS.camera.position.y = 200;
+    PS.camera.position.z = 500;
+
+    PS.cyl.scale.x = 1.0;
+    setSurface(PS.cyl);
+}
+
+function setEllipse()
+{
+    PS.camera.position.x = 0.1;
+    PS.camera.position.y = 200;
+    PS.camera.position.z = 500;
+
+    PS.cyl.scale.x = .6;
+    setSurface(PS.cyl);
+}
+
+function toggleFullScreen()
+{
+    report("toggleFullScreen");
+    if (!THREEx.FullScreen.activated()) {
+        report("setFullScreen");
+        THREEx.FullScreen.request();
+    }
+    else {
+        report("clearFullScreen");
+        THREEx.FullScreen.cancel();
+    }
+}
---
+++ js/Photosphere.js
@@ -0,0 +1,133 @@
+THREE.PhSp = {};
+
+THREE.Photosphere = function (domEl, image, options) {
+	options = options || {};
+
+	var camera, controls, scene, renderer, sphere;
+
+	var webglSupport = (function(){ 
+		try { 
+			var canvas = document.createElement( 'canvas' ); 
+			return !! (window.WebGLRenderingContext && (canvas.getContext('webgl') || canvas.getContext('experimental-webgl'))); 
+		} catch(e) { 
+			return false; 
+		} 
+	})();
+
+	init();
+	render();
+
+	function init () {
+		// http://threejs.org/docs/#Reference/Cameras/PerspectiveCamera
+		camera = new THREE.PerspectiveCamera(options.view || 75, domEl.offsetWidth / domEl.offsetHeight, 1, 1000);
+		camera.position.x = 0.1;
+		camera.position.y = options.y || 0;
+	        camera.position.y = 200;
+	        camera.position.z = 500;
+	    
+                if (options.useGyro) {
+	            controls = new THREE.DeviceOrientationControls( camera );
+	        }
+	        else {
+		    controls = new THREE.OrbitControls(camera);
+                    controls.noPan = true;
+		    controls.noZoom = true; 
+		    controls.autoRotate = true;
+		    controls.autoRotateSpeed = options.speed || 0.5;
+		    controls.addEventListener('change', render);
+                }
+		scene = new THREE.Scene();
+
+		var texture = THREE.ImageUtils.loadTexture(image);
+		texture.minFilter = THREE.LinearFilter;
+	        var material = new THREE.MeshBasicMaterial({
+				map: texture
+                      		});
+	        sphere = new THREE.Mesh(
+		    //new THREE.SphereGeometry(100, 20, 20),
+		    new THREE.SphereGeometry(500, 40, 40),
+		    material
+		);
+	        sphere.scale.x = -1;
+                sphere.name = "sphere";
+
+	        cyl = new THREE.Mesh(
+		    //new THREE.SphereGeometry(100, 20, 20),
+		    //new THREE.SphereGeometry(500, 40, 40),
+		    //new THREE.CylinderGeometry(500,500, 500, 40, 40, true),
+		    //new THREE.CylinderGeometry(500,100, 500, 40, 40, true),
+		    new THREE.CylinderGeometry(100, 800, 500, 60, 40, true), //Good
+		    //new THREE.CylinderGeometry(300, 800, 500, 60, 40, true), //Good
+		    material
+		);
+	        //cyl.scale.x = .8;
+	        cyl.scale.x = .6;
+	        cyl.scale.y = .4;
+                cyl.name = "cyl";
+
+                surface = cyl;
+		scene.add(surface);
+
+		renderer = webglSupport ? new THREE.WebGLRenderer() : new THREE.CanvasRenderer();
+		renderer.setSize(domEl.offsetWidth, domEl.offsetHeight);		
+
+		domEl.appendChild(renderer.domElement);
+
+		domEl.addEventListener('mousewheel', onMouseWheel, false);
+		domEl.addEventListener('DOMMouseScroll', onMouseWheel, false);
+
+	        THREE.PhSp.material = material;
+                THREE.PhSp.scene = scene;
+                THREE.PhSp.camera = camera;
+	        THREE.PhSp.surface = surface;
+	        THREE.PhSp.sphere = sphere;
+	        THREE.PhSp.cyl = cyl;
+                THREE.PhSp.texture = texture;
+	        THREE.PhSp.controls = controls;
+	        THREE.PhSp.renderer = renderer;
+		animate();
+	}
+
+	function render () {
+		renderer.render(scene, camera);
+	}
+
+	function animate () {
+		requestAnimationFrame(animate);
+		controls.update();
+	}
+
+	function onMouseWheel (evt) {
+		evt.preventDefault();
+			
+		if (evt.wheelDeltaY) { // WebKit
+			camera.fov -= evt.wheelDeltaY * 0.05;
+		} else if (evt.wheelDelta) { 	// Opera / IE9
+			camera.fov -= evt.wheelDelta * 0.05;
+		} else if (evt.detail) { // Firefox
+			camera.fov += evt.detail * 1.0;
+		}
+		camera.fov = Math.max(20, Math.min(100, camera.fov));
+		camera.updateProjectionMatrix();
+	}
+
+	function resize () {
+		camera.aspect = domEl.offsetWidth / domEl.offsetHeight;
+		camera.updateProjectionMatrix();
+		renderer.setSize(domEl.offsetWidth, domEl.offsetHeight);
+		render();
+	}
+
+	// http://stackoverflow.com/questions/21548247/clean-up-threejs-webgl-contexts
+	function remove () {
+		scene.remove(sphere);
+		while (domEl.firstChild) {
+			domEl.removeChild(domEl.firstChild);
+		}
+	}
+
+	return {
+		resize: resize,
+		remove: remove
+	}
+};
